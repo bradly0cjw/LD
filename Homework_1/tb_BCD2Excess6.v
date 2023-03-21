17b620b8894b00e9fc13adcdc80b5492ae3f660a
bradly0cjw <bradly093@gmail.com>
2023-03-21 17:23:29 +0800

    fix err

--- Homework_1/tb_BCD2Excess6.v
+++ Homework_1/tb_BCD2Excess6.v
@@ -1,7 +1,7 @@
 module tb_BCD2Excess6;
 
-  reg [0:3] I;
-  wire [0:3] O;
+  reg [3:0] I;
+  wire [3:0] O;
 
   BCD2Excess6 uut(
     .A(I[0]),
@@ -14,17 +14,18 @@ module tb_BCD2Excess6;
     .Z(O[3])
   );
 
-  initial begin
-    #10 I = 4'h0
-    #10 I = 4'h9
-    #10 I = 4'h6
-    #10 I = 4'hf
-    #10 I = 4'h1
-    // #10 A = 0 ; B = 0 ; C = 0 ; D = 0;
-    // #10 A = 1 ; B = 0 ; C = 0 ; D = 1;
-    // #10 A = 0 ; B = 1 ; C = 1 ; D = 0;
-    // #10 A = 1 ; B = 1 ; C = 1 ; D = 1;
-  end
-initial #50 $finish;
+  initial 
+    begin
+      #10 I = 4'h0;
+      #10 I = 4'h9;
+      #10 I = 4'h6;
+      #10 I = 4'hf;
+      #10 I = 4'h1;
+      // #10 A = 0 ; B = 0 ; C = 0 ; D = 0;
+      // #10 A = 1 ; B = 0 ; C = 0 ; D = 1;
+      // #10 A = 0 ; B = 1 ; C = 1 ; D = 0;
+      // #10 A = 1 ; B = 1 ; C = 1 ; D = 1;
+    end
+  initial #50 $finish;
 
 endmodule
