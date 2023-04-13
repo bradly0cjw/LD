5299908488fc55b6bdf69cfb78a6ac0aebd94507
bradly0cjw <bradly093@gmail.com>
2023-04-13 15:19:07 +0800

    update

---
+++ Homework2/HW2_3/Full_adder.v
@@ -0,0 +1,15 @@
+module Full_adder(
+    input x, y, z,
+    output S, C
+);
+    wire [7:0] D_wire;
+
+    Decoder_3x8 uut(
+        .x(x),
+        .y(y),
+        .z(z),
+        .D(D_wire)
+    );
+    assign {S}=D_wire[1]||D_wire[2]||D_wire[4]||D_wire[7];
+    assign {C}=D_wire[3]||D_wire[5]||D_wire[6]||D_wire[7];
+endmodule
---
+++ Homework2/HW2_3/tb_Full_adder.v
@@ -0,0 +1,16 @@
+module tb_Full_adder();
+    reg [2:0] A;
+    wire S,C;
+
+    Full_adder uut(A[2],A[1],A[0],S,C); 
+initial begin
+    A=0;
+    repeat(7)
+    #20 A=A+3'h1;
+end
+initial #150 $finish;
+initial begin
+    $display ("                  time   Select  Carry Sum");
+    $monitor ($time, "    %b       %b  %b",A,C,S);
+end
+endmodule
