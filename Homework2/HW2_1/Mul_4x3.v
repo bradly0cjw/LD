e8fb89da9411139373ed3e49ea71d8f73ab85c13
bradly0cjw <bradly093@gmail.com>
2023-04-14 16:20:43 +0800

    delete unuse line

--- Homework2/HW2_1/Mul_4x3.v
+++ Homework2/HW2_1/Mul_4x3.v
@@ -1,13 +1,12 @@
-module Mul_4x3(
+module Mul_4X2(
     input[2:0]A,
     input[3:0]B,
     output[6:0]C
     );
     wire[2:0]X1;
     wire[3:0]Y1;
-    wire[2:0]X2;
+    wire[3:0]X2;
     wire[3:0]Y2;
-    wire[3:0]X3;
     and G1(C[0],B[0],A[0]);
     and G2(X1[0],B[1],A[0]);
     and G3(X1[1],B[2],A[0]);
@@ -21,8 +20,8 @@ module Mul_4x3(
     and G11(Y2[2],B[2],A[2]);
     and G12(Y2[3],B[3],A[2]);
     ripple_carry_4_bit_adder Adder1(
-            .S({X3[2],X3[1],X3[0],C[1]}),
-            .C4(X3[3]),
+            .S({X2[2],X2[1],X2[0],C[1]}),
+            .C4(X2[3]),
             .A({0,X1[2],X1[1],X1[0]}),
             .B(Y1),
             .C0(0)
@@ -30,7 +29,7 @@ module Mul_4x3(
     ripple_carry_4_bit_adder Adder2(
             .S({C[5],C[4],C[3],C[2]}),
             .C4(C[6]),
-            .A(X3),
+            .A(X2),
             .B(Y2),
             .C0(0)
         );
