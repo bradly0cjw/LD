5f47da6a10214fd979f3ffd2498979300429e55d
bradly0cjw <bradly093@gmail.com>
2023-05-27 22:36:34 +0800

    [refactor] remove unused state

--- Homework3/problem2/problem2.v
+++ Homework3/problem2/problem2.v
@@ -2,7 +2,7 @@ module problem2(x,y,clk,rst);
 input x,clk,rst;
 output y;
 reg [2:0]state;
-parameter S0=3'b000,S1=3'b001,S2=3'b010,S3=3'b011,S4=3'b100,S5=3'b101,S6=3'b110,S7=3'b111;
+parameter S0=3'b000,S1=3'b001,S2=3'b010,S3=3'b011,S4=3'b100,S5=3'b101,S6=3'b110;
 always@(posedge clk,negedge rst)
     if(rst==0)state<=S0;
     else case(state)
